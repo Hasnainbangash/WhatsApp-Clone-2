bc2e6bcc1a0cdf51af9f7dbda6ab96aa5b6dc835
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-19 11:49:29 +0500

    Some isues resolved

--- WhatsApp Clone/Controllers/GroupChatScreenViewController.swift
+++ WhatsApp Clone/Controllers/GroupChatScreenViewController.swift
@@ -74,6 +74,8 @@ class GroupChatScreenViewController: UIViewController {
             .addSnapshotListener { querySnapshot, error in
                 if let e = error {
                     print("There was an issue retrieving data from Firestore: \(e)")
+                    print("AS there is a error so fetching the data from the core data now")
+                    self.fetchMessagesFromCoreData()
                 } else {
                     self.groupMessageChats.removeAll()
                     if let snapshotDocuments = querySnapshot?.documents {
@@ -110,10 +112,10 @@ class GroupChatScreenViewController: UIViewController {
     func loadMessages() {
         fetchMessagesFromFirestore()
         
-        if groupMessageChats.isEmpty {
-            // Fetching the data from the core data
-            fetchMessagesFromCoreData()
-        }
+//        if groupMessageChats.isEmpty {
+//            // Fetching the data from the core data
+//            fetchMessagesFromCoreData()
+//        }
     }
     
     
